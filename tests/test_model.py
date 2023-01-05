d66966ab3982a288a7d70cf23d8b9fcba344f675
Jasper <jasper.derikx@politie.nl>
2023-01-05 19:47:47 +0100

    rephrase exercise 4:

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,6 @@
 # -- Exercise 4 --
 # Write a module scoped fixture that yields an instance of DishwasherModel, trained on the labelled data in
-# dummy_dishwasher_registration.csv. Make it depend on the fixture restrict_grid_search().
+# dummy_dishwasher_registration.csv (using get_trainings_data()). Let it depend on the fixture restrict_grid_search().
 
 
 # -- Exercise 5 --
